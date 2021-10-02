dd621ddfbd031117da55e286dbb494c51249fd99
Brandon Ly <wowbob396@gmail.com>
2021-10-01 21:52:38 -0500

    add victory and spring

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-
+import { withDesign } from 'storybook-addon-designs';
 import { Header } from './Header';
 
 export default {
   title: 'Example/Header',
   component: Header,
+  decorators: [withDesign],
 } as ComponentMeta<typeof Header>;
 
 const Template: ComponentStory<typeof Header> = (args) => <Header {...args} />;
 
 export const LoggedIn = Template.bind({});
+
 LoggedIn.args = {
   user: {},
+
 };
 
 export const LoggedOut = Template.bind({});
 LoggedOut.args = {};
+
+LoggedOut.parameters = {
+  design: {
+    type: 'figma'
+  }
+}
